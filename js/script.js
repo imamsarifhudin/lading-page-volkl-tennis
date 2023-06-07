352a02bc6f48dfcbcbe683d56a162a0aa22b075c
imamsarifhudin <imamsarifhudin8723@gmail.com>
2023-06-07 20:25:56 +0700

    fix back to top

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ function scrollToTop(){
     });
 
 }
+
+ function toggleScroll() {
+     var scrollButton = document.getElementById('scrollTop');
+     var currentScroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+     if (currentScroll > 0) {
+         scrollToTop();
+     }
+
+     window.addEventListener('scroll', function () {
+         var scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+         if (scrollPosition > 0) {
+             scrollButton.style.opacity = 1;
+         } else {
+             scrollButton.style.opacity = 0;
+         }
+     });
+ }
